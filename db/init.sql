842e843023c7c61e12114a4419d53086d0bc239c
lrgsouza <lucas_souza@outlook.com>
2024-09-13 21:02:13 -0300

    Fix init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,58 +1,58 @@
 
-DROP TABLE IF EXISTS questionscore;
-DROP TABLE IF EXISTS surveyquestions;
-DROP TABLE IF EXISTS question;
-DROP TABLE IF EXISTS survey;
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS "questionscore";
+DROP TABLE IF EXISTS "surveyquestions";
+DROP TABLE IF EXISTS "question";
+DROP TABLE IF EXISTS "survey";
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "department";
+DROP TABLE IF EXISTS "organization";
 
 
-CREATE TABLE organization (
+CREATE TABLE "organization" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE department (
+CREATE TABLE "department" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    orgId INT REFERENCES Organization(id)
+    orgId INT REFERENCES "organization"(id)
 );
 
-CREATE TABLE user (
+CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    deptId INT REFERENCES Department(id),
-    orgId INT REFERENCES Organization(id),
+    deptId INT REFERENCES "department"(id),
+    orgId INT REFERENCES "organization"(id),
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     type VARCHAR(50) CHECK (type IN ('employee', 'admin')) NOT NULL
 );
 
-CREATE TABLE survey (
+CREATE TABLE "survey" (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    orgId INT REFERENCES Organization(id)
+    orgId INT REFERENCES "organization"(id)
 );
 
-CREATE TABLE question (
+CREATE TABLE "question" (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE surveyquestions (
-    surveyId INT REFERENCES Survey(id) ON DELETE CASCADE,
-    questionId INT REFERENCES Question(id) ON DELETE CASCADE,
+CREATE TABLE "surveyquestions" (
+    surveyId INT REFERENCES "survey"(id) ON DELETE CASCADE,
+    questionId INT REFERENCES "questionscoreuestion"(id) ON DELETE CASCADE,
     PRIMARY KEY (surveyId, questionId)
 );
 
-CREATE TABLE questionscore (
-    employeeId INT REFERENCES "User"(id),
-    questionId INT REFERENCES Question(id),
+CREATE TABLE "questionscore" (
+    employeeId INT REFERENCES "user"(id),
+    questionId INT REFERENCES "question"(id),
     score INT NOT NULL,
     PRIMARY KEY (employeeId, questionId)
 );
 
-CREATE INDEX idx_user_orgId ON "User"(orgId);
-CREATE INDEX idx_user_deptId ON "User"(deptId);
-CREATE INDEX idx_survey_orgId ON Survey(orgId);
+CREATE INDEX idx_user_orgId ON "user"(orgId);
+CREATE INDEX idx_user_deptId ON "user"(deptId);
+CREATE INDEX idx_survey_orgId ON "survey"(orgId);
