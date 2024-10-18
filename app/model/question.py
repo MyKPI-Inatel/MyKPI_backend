3f664e81a030629c9e11f52a4a1a622a5c0dbd89
airtong <airton-carvalho@outlook.com>
2024-10-17 23:37:17 -0300

    [feature] create question requires survey id

--- app/model/question.py
+++ app/model/question.py
@@ -5,11 +5,14 @@ class QuestionBase(BaseModel):
     id: int
     title: str
     scorefactor: int
+    surveyid: Optional[int] = None
 
 class QuestionCreate(BaseModel):
     title: str
     scorefactor: int
+    surveyid: int
 
 class QuestionUpdate(BaseModel):
     title: Optional[str] = None
     scorefactor: Optional[int] = None
+    surveyid: Optional[int] = None
--- app/service/question.py
+++ app/service/question.py
@@ -1,10 +1,16 @@
 from model.question import QuestionCreate, QuestionUpdate, QuestionBase
 from dao.question import QuestionDAO
+from dao.surveyquestion import SurveyQuestionDAO
 
 class Question:
     @staticmethod
     async def create_question(question_data: QuestionCreate) -> QuestionBase:
         new_question_data = await QuestionDAO.insert(question_data)
+
+        await SurveyQuestionDAO.insert(question_data.surveyid, new_question_data.id)
+
+        new_question_data.surveyid = question_data.surveyid
+        
         return new_question_data
 
     @staticmethod
@@ -15,6 +21,7 @@ class Question:
     @staticmethod
     async def get_question(questionid: int) -> QuestionBase:
         question_data = await QuestionDAO.get(questionid)
+
         return question_data
 
     @staticmethod
