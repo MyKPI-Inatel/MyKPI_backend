e2fd8259cedc334d7afb223457cc6a2677f57fb9
airtong <airton-carvalho@outlook.com>
2024-10-19 01:25:02 -0300

    [test] get all orgs unit test

--- app/test/test_service_organization.py
+++ app/test/test_service_organization.py
@@ -8,8 +8,8 @@ from dao.organization import OrganizationDAO
 @pytest.mark.org
 async def test_create_organization(mocker):
     # Mock input and expected return values
-    organization_data = OrganizationCreate(name="HR")
-    expected_return = OrganizationBase(id=1, name="HR")
+    organization_data = OrganizationCreate(name="Inatel")
+    expected_return = OrganizationBase(id=1, name="Inatel")
 
     # Mock the insert method of the DAO
     mocker.patch.object(OrganizationDAO, 'insert', new_callable=AsyncMock, return_value=expected_return)
@@ -28,7 +28,7 @@ async def test_create_organization(mocker):
 async def test_get_organization(mocker):
     # Mock input and expected return values
     organizationid = 1
-    expected_return = OrganizationBase(id=1, name="HR")
+    expected_return = OrganizationBase(id=1, name="Inatel")
 
     # Mock the get method of the DAO
     mocker.patch.object(OrganizationDAO, 'get', new_callable=AsyncMock, return_value=expected_return)
@@ -38,6 +38,24 @@ async def test_get_organization(mocker):
 
     # Asserts that the DAO's get method was called with the correct input
     OrganizationDAO.get.assert_called_once_with(organizationid)
+
+    # Asserts that the result is as expected
+    assert result == expected_return
+
+@pytest.mark.asyncio
+@pytest.mark.org
+async def test_get_all_organizations(mocker):
+    # Mock expected return values
+    expected_return = [OrganizationBase(id=1, name="INATEL"), OrganizationBase(id=2, name="MyKPI")]
+
+    # Mock the get_all method of the DAO
+    mocker.patch.object(OrganizationDAO, 'get_all', new_callable=AsyncMock, return_value=expected_return)
+
+    # Call the function we're testing
+    result = await Organization.get_all_organizations()
+
+    # Asserts that the DAO's get_all method was called
+    OrganizationDAO.get_all.assert_called_once()
 
     # Asserts that the result is as expected
     assert result == expected_return
