01e61febfa6f81854ace2beed400e4776be73c78
airtong <airton-carvalho@outlook.com>
2024-10-21 18:22:29 -0300

    [test] get department api test

--- app/test/test_api_department.py
+++ app/test/test_api_department.py
@@ -31,3 +31,22 @@ async def test_create_department(reset_database):
             "name": "RH",
             "orgid": 1
         }
+
+@pytest.mark.asyncio
+@pytest.mark.dept
+@pytest.mark.functional
+async def test_get_department(reset_database):
+    async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
+
+        # Send a GET request to create a department
+        response = await client.get("/api/v1/departments/org/1/1")
+
+        # Assert the response status code
+        assert response.status_code == 200
+        
+        # Assert the returned data matches the expected format
+        assert response.json() == {
+            "id": 1,
+            "name": "Geral",
+            "orgid": 1
+        }
