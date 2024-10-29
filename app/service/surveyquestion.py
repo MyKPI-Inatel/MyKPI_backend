b6d7f3d467230314872611dfa3e1ac34a7a4158d
airtong <airton-carvalho@outlook.com>
2024-10-28 22:32:44 -0300

    [cleanup] remove unused function get all surveyquestions

--- app/service/surveyquestion.py
+++ app/service/surveyquestion.py
@@ -8,11 +8,6 @@ class SurveyQuestion:
         return new_surveyquestion_data
 
     @staticmethod
-    async def get_all_surveyquestions() -> list[SurveyQuestionBase]:
-        surveyquestions_data = await SurveyQuestionDAO.get_all()
-        return surveyquestions_data
-
-    @staticmethod
     async def get_by_survey(surveyid: int) -> SurveyQuestionBase:
         surveyquestion_data = await SurveyQuestionDAO.get_by_survey(surveyid)
         return surveyquestion_data
