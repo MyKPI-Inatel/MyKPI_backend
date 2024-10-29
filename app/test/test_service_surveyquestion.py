0b102744bf73d1019f04896b677dc503a83dcf2d
airtong <airton-carvalho@outlook.com>
2024-10-28 22:29:09 -0300

    [test] get surveyquestion bt survey test

--- app/test/test_service_surveyquestion.py
+++ app/test/test_service_surveyquestion.py
@@ -30,8 +30,8 @@ async def test_svc_create_surveyquestion(mocker):
 @pytest.mark.unit
 async def test_svc_get_surveyquestion_by_question(mocker):
     # Mock input and expected return values
-    questionid = 1
-    expected_return = SurveyQuestionBase(surveyid=2, questionid=3)
+    questionid = 5
+    expected_return = [SurveyQuestionBase(surveyid=2, questionid=questionid)]
 
     # Mock the get method of the DAO
     mocker.patch.object(SurveyQuestionDAO, 'get_by_question', new_callable=AsyncMock, return_value=expected_return)
@@ -41,6 +41,29 @@ async def test_svc_get_surveyquestion_by_question(mocker):
 
     # Asserts that the DAO's get method was called with the correct input
     SurveyQuestionDAO.get_by_question.assert_called_once_with(questionid)
+
+    # Asserts that the result is as expected
+    assert result == expected_return
+
+
+@pytest.mark.asyncio
+@pytest.mark.survquest
+@pytest.mark.unit
+async def test_svc_get_surveyquestion_by_survey(mocker):
+    # Mock input and expected return values
+    surveyid = 1
+    expected_return = [SurveyQuestionBase(surveyid=surveyid, questionid=2),
+                       SurveyQuestionBase(surveyid=surveyid, questionid=3),
+                       SurveyQuestionBase(surveyid=surveyid, questionid=4)]
+
+    # Mock the get method of the DAO
+    mocker.patch.object(SurveyQuestionDAO, 'get_by_survey', new_callable=AsyncMock, return_value=expected_return)
+
+    # Call the function we're testing
+    result = await SurveyQuestion.get_by_survey(surveyid)
+
+    # Asserts that the DAO's get method was called with the correct input
+    SurveyQuestionDAO.get_by_survey.assert_called_once_with(surveyid)
 
     # Asserts that the result is as expected
     assert result == expected_return
