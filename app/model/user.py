950acd5056781a5f535c857a5531aea918c9b1cc
airtong <airton-carvalho@outlook.com>
2024-11-01 02:45:33 -0300

    [feature] add new models for user

--- app/model/user.py
+++ app/model/user.py
@@ -1,25 +1,40 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Literal, Optional
 
 class UserBase(BaseModel):
     id: Optional[int] = None
     email: str
     name: str
     password: str
-    usertype: str
+    usertype: Literal['employee', 'orgadmin', 'superadmin'] = Field(
+        ..., 
+        description="User type can be one of the following: 'employee', 'orgadmin', 'superadmin'"
+    )
     orgid: int
     deptid: int
-
-    def toJSON(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            "name": self.name,
-            "usertype": self.usertype,
-            "orgid": self.orgid,
-            "deptid": self.deptid
-        }
 
 class UserLogin(BaseModel):
     email: str
     password: str
+
+class UserCreate(BaseModel):
+    email: str
+    name: str
+    password: str
+    usertype: Literal['employee', 'orgadmin', 'superadmin'] = Field(
+        ..., 
+        description="User type can be one of the following: 'employee', 'orgadmin', 'superadmin'"
+    )
+    orgid: int
+    deptid: int
+
+class UserUpdate(BaseModel):
+    name: Optional[str]
+    password: Optional[str]
+    email: Optional[str]
+    usertype: Optional[Literal['employee', 'orgadmin', 'superadmin']] = Field(
+        ..., 
+        description="User type can be one of the following: 'employee', 'orgadmin', 'superadmin'"
+    )
+    orgid: Optional[int]
+    deptid: Optional[int]
