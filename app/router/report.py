1f5abff325164dc23743cd78325046c433399786
lrgsouza <lucas_souza@outlook.com>
2024-11-04 16:05:24 -0300

    [security] Add permission to report path

--- app/router/report.py
+++ app/router/report.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from typing import List
 from service.report import Report as ReportService
 from fastapi import APIRouter
+from service.user import User
+from model.user import UserType
+from internal.security import get_current_user, verify_permissions
+from service.survey import Survey as SurveyService
 
 router = APIRouter()
 
@@ -11,6 +15,11 @@ router = APIRouter()
     summary="Retrieve all reports", 
     description="Retrieve a list of all reports available."
 )
-async def get_reports(surveyid: int):
+async def get_reports(surveyid: int,
+    current_user: User = Depends(get_current_user)):
+
+    survey = await SurveyService.get_survey(surveyid)
+
+    verify_permissions(current_user, UserType.orgadmin, {'orgid': survey.orgid})
     reports = await ReportService.get_report_by_surveyid(surveyid)
     return reports
