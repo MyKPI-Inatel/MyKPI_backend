196356dbefc2b2666d96f1d9f73d88b58e256035
airtong <airton-carvalho@outlook.com>
2024-11-14 22:39:56 -0300

    [doc] update user router docs

--- app/router/user.py
+++ app/router/user.py
@@ -34,11 +34,11 @@ async def create_user(user: UserCreate):
     return await User.create_user(user)
 
 @router.post('/employee', 
-             status_code=HTTPStatus.CREATED, 
-             response_model=UserBase,
-             summary="Create a new employee, you must be an organization admin",
-             description="This endpoint allows you to create a new employee, you must be an organization admin. You can only create employees for your organization."
-             )
+    status_code=HTTPStatus.CREATED, 
+    response_model=UserBase,
+    summary="Create a new employee, you must be an organization admin",
+    description="This endpoint allows you to create a new employee, you must be an organization admin. You can only create employees for your organization."
+)
 async def create_user(user: EmployeeCreate, 
                       current_user: CurrentUser = Depends(get_current_user)):
 
@@ -67,10 +67,11 @@ async def create_user(user: EmployeeCreate,
     return await User.create_user(user)
 
 @router.put('/users/{user_id}/', 
-            status_code=HTTPStatus.OK,
-            response_model=UserBase,
-            summary="Update a user",
-            )
+    status_code=HTTPStatus.OK,
+    response_model=UserBase,
+    summary="Update a user",
+    description="Update the details of a specific user by its ID."
+)
 def update_user(
     user_id: int,
     user: UserUpdate,
@@ -87,7 +88,13 @@ def update_user(
         raise HTTPException(HTTPStatus.CONFLICT, 'Email already exists') from e
 
 
-@router.delete('/users/{user_id}')
+@router.delete(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=dict(message=str),
+    summary="Delete a user",
+    description="Delete a specific user by its ID."
+)
 async def delete_user(
     user_id: int,
     current_user: CurrentUser = Depends(get_current_user)
@@ -101,7 +108,12 @@ async def delete_user(
     return {'message': 'User deleted successfully'}
 
 
-@router.post('/login', response_model=Token)
+@router.post('/login',
+    status_code=HTTPStatus.OK,
+    response_model=Token,
+    summary="Login for access token",
+    description="Login for access token"
+)
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends()
 ):
@@ -121,7 +133,12 @@ async def login_for_access_token(
     return {'access_token': access_token, 'token_type': 'bearer'}
 
 # get users by orgid
-@router.get('/users', response_model=list[EmployeeBase])
+@router.get('/users',
+    status_code=HTTPStatus.OK,
+    response_model=list[EmployeeBase],
+    summary="Get all users by organization ID",
+    description="Retrieve a list of all users associated with a specific organization ID."
+)
 async def get_users_by_orgid(
     current_user: CurrentUser = Depends(get_current_user)
 ):
