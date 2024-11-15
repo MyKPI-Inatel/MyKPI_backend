356f5eb537ba08c632cae0d4dda99440d9490bea
airtong <airton-carvalho@outlook.com>
2024-11-15 18:23:09 -0300

    [test] fix expected http code

--- app/test/test_api_question.py
+++ app/test/test_api_question.py
@@ -26,7 +26,7 @@ async def test_api_create_question(reset_database):
         response = await client.post("/api/v1/questions/", json=question_data)
 
         # Assert the response status code
-        assert response.status_code == HTTPStatus.OK
+        assert response.status_code == HTTPStatus.CREATED
         
         # Assert the returned data matches the expected format
         assert response.json() == {
