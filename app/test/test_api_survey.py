45621194f874e6009a10f3f8ad706e15939880fd
airtong <airton-carvalho@outlook.com>
2024-11-16 19:33:27 -0300

    [test] get survey api test

--- app/test/test_api_survey.py
+++ app/test/test_api_survey.py
@@ -56,3 +56,20 @@ async def test_api_get_surveys(reset_database, access_token_superadmin):
         
         response_json = response.json()
         assert isinstance(response_json, list)
+
+@pytest.mark.asyncio
+@pytest.mark.survey
+@pytest.mark.functional
+async def test_api_get_survey(reset_database, access_token_orgadmin):
+    async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
+        headers = {"Authorization": f"Bearer {access_token_orgadmin}"}
+        survey_data = {"title": "Survey at Inatel", "orgid": 2}
+        create_response = await client.post("/api/v1/surveys/", json=survey_data, headers=headers)
+        survey_id = create_response.json()["id"]
+
+        response = await client.get(f"/api/v1/surveys/{survey_id}", headers=headers)
+        assert response.status_code == HTTPStatus.OK
+        
+        response_json = response.json()
+        assert response_json["title"] == "Survey at Inatel"
+        assert response_json["orgid"] == 2
