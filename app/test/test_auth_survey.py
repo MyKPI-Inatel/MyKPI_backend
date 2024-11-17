7d9572bd5e5d5a218b44e1f821efc82591bc6b09
airtong <airton-carvalho@outlook.com>
2024-11-16 21:48:55 -0300

    [test] create survey auth test

--- app/test/test_auth_survey.py
+++ app/test/test_auth_survey.py
@@ -8,8 +8,22 @@ from main import appServer
 @pytest.mark.survey
 @pytest.mark.functional
 @pytest.mark.auth
+@pytest.mark.employee
+async def test_auth_create_survey_employee(access_token):
+    async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
+        survey_data = {"title": "Survey at Inatel", "orgid": 2}
+        headers = {"Authorization": f"Bearer {access_token}"}
+        
+        response = await client.post("/api/v1/surveys/", json=survey_data, headers=headers)
+        assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+@pytest.mark.asyncio
+@pytest.mark.survey
+@pytest.mark.functional
+@pytest.mark.auth
 @pytest.mark.orgadmin
-async def test_api_auth_get_surveys_orgadmin(access_token):
+async def test_auth_get_surveys_orgadmin(access_token):
     async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
         headers = {"Authorization": f"Bearer {access_token}"}
         response = await client.get("/api/v1/surveys/", headers=headers)
@@ -21,7 +35,7 @@ async def test_api_auth_get_surveys_orgadmin(access_token):
 @pytest.mark.functional
 @pytest.mark.auth
 @pytest.mark.employee
-async def test_api_auth_get_surveys_employee(access_token):
+async def test_auth_get_surveys_employee(access_token):
     async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
         headers = {"Authorization": f"Bearer {access_token}"}
         response = await client.get("/api/v1/surveys/", headers=headers)
