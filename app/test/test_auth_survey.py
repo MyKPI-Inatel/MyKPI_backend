1c9683ec3f23e8e716b6124e28480a99dd5fc3f0
airtong <airton-carvalho@outlook.com>
2024-11-16 22:22:52 -0300

    [test] create survey wrong org auth test

--- app/test/test_auth_survey.py
+++ app/test/test_auth_survey.py
@@ -35,6 +35,20 @@ async def test_auth_create_survey_unauthenticated():
 @pytest.mark.functional
 @pytest.mark.auth
 @pytest.mark.orgadmin
+async def test_api_create_survey_wrong_org(access_token):
+    async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
+        survey_data = {"title": "Survey at Inatel", "orgid": 3}
+        headers = {"Authorization": f"Bearer {access_token}"}
+        
+        response = await client.post("/api/v1/surveys/", json=survey_data, headers=headers)
+        assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+@pytest.mark.asyncio
+@pytest.mark.survey
+@pytest.mark.functional
+@pytest.mark.auth
+@pytest.mark.orgadmin
 async def test_auth_get_surveys_orgadmin(access_token):
     async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
         headers = {"Authorization": f"Bearer {access_token}"}
