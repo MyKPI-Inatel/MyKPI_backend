3a3363fa42b58700980a4f4bf844dcffcd8d0229
airtong <airton-carvalho@outlook.com>
2024-11-16 21:53:16 -0300

    [test] get surveys by org auth test

--- app/test/test_auth_survey.py
+++ app/test/test_auth_survey.py
@@ -45,6 +45,18 @@ async def test_auth_get_survey_employee(access_token):
 @pytest.mark.asyncio
 @pytest.mark.survey
 @pytest.mark.functional
+@pytest.mark.orgadmin
+async def test_api_get_surveys_by_org(access_token):
+    async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        response = await client.get("/api/v1/surveys/org/2", headers=headers)
+        assert response.status_code == HTTPStatus.FORBIDDEN
+
+
+@pytest.mark.asyncio
+@pytest.mark.survey
+@pytest.mark.functional
 @pytest.mark.auth
 @pytest.mark.employee
 async def test_auth_get_surveys_employee(access_token):
