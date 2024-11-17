3c7944afeba2aa4ac547b216f02b50dd03fd1dc0
airtong <airton-carvalho@outlook.com>
2024-11-16 22:07:41 -0300

    [auth] create survey unauth test

--- app/test/test_auth_survey.py
+++ app/test/test_auth_survey.py
@@ -22,6 +22,18 @@ async def test_auth_create_survey_employee(access_token):
 @pytest.mark.survey
 @pytest.mark.functional
 @pytest.mark.auth
+async def test_auth_create_survey_unauthenticated():
+    async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
+        survey_data = {"title": "Survey at Inatel", "orgid": 2}
+        
+        response = await client.post("/api/v1/surveys/", json=survey_data)
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+@pytest.mark.asyncio
+@pytest.mark.survey
+@pytest.mark.functional
+@pytest.mark.auth
 @pytest.mark.orgadmin
 async def test_auth_get_surveys_orgadmin(access_token):
     async with AsyncClient(transport=ASGITransport(app=appServer), base_url="http://test") as client:
